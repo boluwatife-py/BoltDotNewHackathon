6fad6bb87d567d3e6ece5f8dd437a8feea917cc2
Boluwatife (SoftTouch) <boluwatifeakingbade28@gmail.com>
2025-06-25 02:23:17 +0100

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -72,7 +72,7 @@ const Home: React.FC = () => {
         <NoticeCard />
 
         <div className="px-[var(--lg)] py-[var(--slg)] text-center">
-          <h1 className="text-[28px] font-bold text-[var(--text-primary)] tracking-[-0.5px]">
+          <h1 className="text-[28px] font-bold text-[var(--text-primary)] tracking-[-0.5px] fleex">
             Today,
             <span className="block text-[12px] font-medium">{formattedDate}</span>
           </h1>
